e7f4355c063b3bae3b269250f7d33f75771e085c
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-01-24 23:48:26 -0300

    Criação Projeto WebSite

---
+++ Courses/WhaeverAplic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhaeverAplic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
