8dc7877ccdb66f6e560f97b177718c1c13ef75ed
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-01-28 23:39:43 -0300

    Family Member

--- Courses/Fundamentals/Program.cs
+++ Courses/Fundamentals/Program.cs
@@ -5,4 +5,7 @@ string BirthDay = "";
 int age = 0;
 
 double weight = 0;
-
+IList<string> FamilyMember = new List<string>();
+FamilyMember.Add("Father");
+FamilyMember.Add("Mother");
+FamilyMember.Add("Brother");
